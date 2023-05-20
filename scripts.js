10460cbb1591d49378646ba317f83b8dfce744ad
alessio995 <131875097+alessio995@users.noreply.github.com>
2023-05-21 00:27:14 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -668,7 +668,7 @@ function mostraFormazioniLive() {
     formazione2: {
       titolari: ["P - RUI PATRICIO", "D - CAMBIASO", "D - DODO D", "D - CELIK", "C - CALHANOGLU", "C - TONALI", "C - FELIPE ANDERSON", "C - RICCI S", "A - BERARDI", "A - VLAHOVIC", "A - DYBALA"],
       panchinari: ["P - BOER", "P - SVILAR", "D - CUADRADO", "D - HATEBOER", "D - DJIMSITI", "D - LYKOGIANNIS", "C - MIRANCHUK", "C - DJURICIC", "C - BREKALO", "C - SENSI", "A - CIURRIA", "A - REBIC", "A - SHOMURODOV", "A - SANABRIA"],
-      voti: [0,6.5,0,0,8.5,0,0,0,9.5,0,0,0,0,0,0,6.5,0,0,0,0,5.5,10,0,0,0]
+      voti: [0,6.5,0,0,0,8.5,0,0,9.5,0,0,0,0,0,0,6.5,0,0,0,0,5.5,10,0,0,0]
     }
   },
     {
@@ -678,7 +678,7 @@ function mostraFormazioniLive() {
       titolari: ["P - SZCZESNY", "D - TOLJAN", "D - DIMARCO", "D - BIJOL", "C - PEREYRA", "C - BAJRAMI", "C - FERGUSON", "C - MESSIAS", "A - LUKAKU R", "A - LEAO", "A - NZOLA"],
       panchinari: ["P - PERIN", "P - ONANA", "D - ZORTEA", "D - MARUSIC", "D - MARIO RUI", "D - BIRINDELLI", "C - KOSTIC", "C - POLITANO", "C - LOBOTKA", "A - BETO", "A - PINAMONTI", "A - PICCOLI"],
 
-      voti: [0, 6, 0, 0, 0,6,10.5,0,6,0,12,0,6,0,0,0,7.5,0,0,0,0,6,0]
+      voti: [0, 6, 0, 0, 6,0,10.5,0,6,12,0,0,6,0,0,0,7.5,0,0,0,0,6,0]
     },
     formazione2: {
        titolari:  ["P - MAIGNAN", "D - SMALLING", "D - HERNANDEZ T", "D - RRAHMANI", "C - MILINKOVIC-SAVIC", "C - FRATTESI", "C - WIJNALDUM", "C - ELMAS", "A - GIROUD", "A - IMMOBILE", "A - ARNAUTOVIC"],
