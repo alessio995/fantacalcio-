a55e457c0e70473a9d5fa9f3b3c75aa5eb3cd67c
alessio995 <131875097+alessio995@users.noreply.github.com>
2023-05-26 19:57:23 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -667,7 +667,7 @@ function mostraFormazioniLive() {
     fantallenatore2: "ASTON BIRRA",
     formazione1: {
       titolari:["P1 - Svilar", "D2 - Dodo D", "D3 - Cuadrado", "D4 - Cambiaso", "C5 - Calhanoglu", "C6 - Tonali", "C7 - Felipe Anderson", "C8 - Miranchuk", "A9 - Berardi", "A10 - Vlahovic", "A11 - Sanabria"],
-      panchinari:["P12 - Rui Patricio", "P13 - Boer", "D14 - Hateboer", "D15 - Celik", "D16 - Djimsiti", "D17 - Lykogiannis", "C18 - Djuricic", "C19 - Sensi", "C20 - Brekalo", "C21 - Ricci S", "A22 - Dybala", "A23 - Ciurria", "A24 - Rebic", "A25 - Shomurodov"]
+      panchinari:["P12 - Rui Patricio", "P13 - Boer", "D14 - Hateboer", "D15 - Celik", "D16 - Djimsiti", "D17 - Lykogiannis", "C18 - Djuricic", "C19 - Sensi", "C20 - Brekalo", "C21 - Ricci S", "A22 - Dybala", "A23 - Ciurria", "A24 - Rebic", "A25 - Shomurodov"],
     //voti: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25]
       voti: [0, 0, 0, 0, 0, 0, 0, 0, 0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0,  0]
     },
